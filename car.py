6ea7972d03a4817f7cd9ef9f8c80d4ea18f7d802
ww550707 <31184509+ww550707@users.noreply.github.com>
2018-04-06 11:09:27 +0800

    led test

--- car.py
+++ car.py
@@ -1,5 +1,16 @@
-print('hello world!')
+import PRi.GPIO
+import time
 
+RPi.GPIO.setmode(RPI.GPIO.BCM)
+RPi.GPIO.setup(14,RPI.GPIO.OUT)
 
+print("start")
 
+for i in range (0,10):
+    RPi.GPIO.output(14,True)
+    time.sleep(0.5)
+    RPi.GPIO.output(14,False)
+    time.sleep(0.5)
 
+print("end")
+RPi.GPIO.cleanup()
