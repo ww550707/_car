e12ed4df1c1c5fa59e445a7e54274d3c8f9b7d8a
ww550707 <31184509+ww550707@users.noreply.github.com>
2018-04-06 13:40:33 +0800

    inupt test

--- car.py
+++ car.py
@@ -2,15 +2,26 @@ import RPi.GPIO
 import time
 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
-RPi.GPIO.setup(14,RPi.GPIO.OUT)
+RPi.GPIO.setup(14,RPi.GPIO.IN)
+
+flag = 0;
+get = 0;
 
 print("start")
 
+while(1):
+    get = GPIO.input(14)
+    if (get == 1 & flag == 0):
+        print("get")
+        flag = 1
+    if (get == 0):
+        flag == 0
+'''
 for i in range (0,10):
     RPi.GPIO.output(14,True)
     time.sleep(0.5)
     RPi.GPIO.output(14,False)
     time.sleep(0.5)
-
+'''
 print("end")
 RPi.GPIO.cleanup()
